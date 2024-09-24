ad3fb072e1d02217324ef4457bda7bd7863acea1
jl2218 <joaopedropepe56@gmail.com>
2024-09-24 17:12:47 -0300

    fix: mensagem de erro

--- src/app/api/users/change-password/route.ts
+++ src/app/api/users/change-password/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: Request) {
         );
     } catch (error : any) {
         return NextResponse.json(
-            { message: 'Erro ao criar usuário', error: error.message },
+            { message: 'Erro ao alterar senha', error: error.message },
             { status: 500 }
         );
     }
