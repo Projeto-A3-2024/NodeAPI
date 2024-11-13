6a4b365132affc1fbce960cec1a13fc0cfcd3630
jl2218 <joaopedropepe56@gmail.com>
2024-11-12 22:32:28 -0300

    fix: swagger.ts

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -6,8 +6,11 @@ export const getApiDocs = async () => {
     definition: {
       openapi: "3.0.0",
       info: {
-        title: "Next Swagger API Example",
+        title: "API de Agendamento",
         version: "1.0",
+        description: `Esta API, desenvolvida com Next.js, permite o gerenciamento completo de agendamentos para profissionais da saúde, oferecendo funcionalidades para criar, listar e gerenciar horários disponíveis, agendamentos e usuários.
+                      Ela oferece um sistema de autenticação via token JWT, com diferentes níveis de acesso para profissionais, pacientes e administradores.
+                      A API também permite a criação de novos profissionais, recuperação de senhas e comunicação com os usuários através de e-mails automatizados para recuperação de acesso.`,
       },
       components: {
         securitySchemes: {
